c279c68606e931264ca57ee7ca0e776c70899cf6
dbeagus <dbeagus@gmail.com>
2024-07-16 16:01:42 -0300

    fix relationship between user and course #11

--- app/models/course_user.py
+++ app/models/course_user.py
@@ -5,7 +5,12 @@ class CourseUser(db.Model):
     
     id = db.Column(db.Integer, primary_key = True)
     grade = db.Column(db.String(4), nullable = True)
+    
     #ForeignKey to Course
     course_id = db.Column(db.Integer, db.ForeignKey("courses.id"), nullable = False)
     #ForeignKey to User
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    #Many to One relationship with User
+    user = db.relationship("User", back_populates = "course_user")
+    #Many to One relationship with Course
+    course = db.relationship("Course", back_populates = "course_user")
