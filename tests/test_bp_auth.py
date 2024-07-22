c1b9b47bbfb494926cffd6e6a4528ba01d3f05a3
dbeagus <dbeagus@gmail.com>
2024-07-22 17:54:25 -0300

    fix test for login #15

--- tests/test_bp_auth.py
+++ tests/test_bp_auth.py
@@ -2,6 +2,7 @@ import unittest
 import os
 from flask import Flask
 from app import db, create_app
+from app.models.profile import Profile
 from app.models.user import User
 from app.utils.initializers import initialize_roles
 
@@ -29,6 +30,10 @@ class TestAuthBP(unittest.TestCase):
         db.session.add(user)
         db.session.commit()
         
+        profile = Profile(user_id = user.id, role_id = 1)
+        db.session.add(profile)
+        db.session.commit()
+        
         data = {
             "email": "test@test.com",
             "password": "test"
@@ -39,9 +44,9 @@ class TestAuthBP(unittest.TestCase):
             "password" : "123"
         }
         
-        response = self.client.post("/auth", json = data)
+        response = self.client.post("/login", json = data)
         self.assertEqual(response.status_code, 200)
         self.assertIn("access_token", response.json)
         
-        response2 = self.client.post("/auth", json = data2)
+        response2 = self.client.post("/login", json = data2)
         self.assertEqual(response2.status_code, 401)
