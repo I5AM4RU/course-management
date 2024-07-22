c72fc0af496f39d44c432df717286b513109bf65
dbeagus <dbeagus@gmail.com>
2024-07-22 16:14:09 -0300

    add test for enroll user #14

--- app/services/fetchers.py
+++ app/services/fetchers.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from app.models.course import Course
+from app.models.course_user import CourseUser
 from app.models.user import User
 from app.models.user_data import UserData
 
@@ -18,3 +19,9 @@ def fetch_user(user_id):
 def fetch_user_data(user_id):
     user_data = UserData.query.filter_by(user_id=user_id).first()
     return user_data
+
+def fetch_course_user(user_id, course_id):
+    course_user = CourseUser.query.filter_by(user_id = user_id, course_id = course_id).first()
+    if not course_user:
+        raise Exception("User not enrolled.")
+    return course_user
